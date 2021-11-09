39d698cd8020b8e270a060b606719e60174fb575
iliuhach627 <iliuhach627@gmail.com>
2021-11-09 14:34:49 +0300

    other

--- src/main/java/com/senla/hotel/Runner.java
+++ src/main/java/com/senla/hotel/Runner.java
@@ -1,7 +1,13 @@
 package com.senla.hotel;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
 public class Runner {
     public static void main(String[] args) {
 
+        SpringApplication.run(Runner.class, args);
     }
 }
